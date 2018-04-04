d72103c5178126e48e3b600a1218b951f5a6cb5d
Dave McCalmont <dave_mccalmont@hotmail.com>
2018-04-03 23:49:12 -0500

    Refactored addItem function to include <label> and indexes

--- JustAChecklist.js
+++ JustAChecklist.js
@@ -2,13 +2,28 @@ const item = document.querySelector('input[type=text]');
 const entryForm = document.querySelector('form');
 const checklist = document.querySelector('.checklist');
 const toDoItems = [];
+// const newItems = document.querySelectorAll('.newItem');
+const boxes = document.querySelectorAll('input[type=checkbox]');
 
 
 function addItem(e){
   e.preventDefault();
   toDoItems.push(item.value);
-  checklist.innerHTML = toDoItems.map(toDoItem => `<li>${toDoItem}</li>`).join('');
+  checklist.innerHTML = toDoItems.map((toDoItem,i) => {
+      return `
+      <li>
+        <input type="checkbox" id="toDoItem${i}" data-index=${i}>
+        <label for="toDoItem${i}">${toDoItem}</label>
+       </li>
+          `;
+      }).join('');
   entryForm.reset();
 };
 
+function strike(e) {
+  console.log(e);
+};
+
+
 entryForm.addEventListener('submit', addItem);
+boxes.forEach(item => addEventListener('click',strike));
