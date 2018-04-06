b14f9db048d356fd7e16c4715b11f3b68dc7d4f3
Dave McCalmont <dave_mccalmont@hotmail.com>
2018-04-06 00:13:12 -0500

    Added function that will move a checked item to a separate list

--- JustAChecklist.js
+++ JustAChecklist.js
@@ -1,7 +1,9 @@
 const item = document.querySelector('input[type=text]');
 const entryForm = document.querySelector('form');
 const checklist = document.querySelector('.checklist');
+const checkedBoxes = document.querySelector('.checkedBoxes');
 const toDoItems = [];
+const compItems = [];
 
 function addItem(e){
   e.preventDefault();
@@ -17,9 +19,34 @@ function addItem(e){
   entryForm.reset();
 };
 
-function strike(e) {
-  console.log(e);
-};
+function checked(e) {
+  let indexChecked = e.target.dataset.index;
+  console.log(indexChecked);
+  if(e.target.checked) {
+    compItems.push(toDoItems[indexChecked]);
+    checkedBoxes.innerHTML = compItems.map((compItem,i) => {
+      return `
+      <li>
+        <input type="checkbox" checked=true id="compItem${i}">
+        <label for="compItem${i}">${compItem}</label>
+      </li>
+          `;
+    }).join('');
 
+    // const leftToDo = [...toDoItems.slice(0,indexChecked),
+    //                   ...toDoItems.slice(indexChecked+1)];
+    // console.log(leftToDo);
+  }
+}
+
+
+// function checked(e) {
+//   if(e.target.checked) {
+//     console.log('I am checked!');
+//   } else {
+//     console.log('I am not checked!');
+//   };
+// };
 
 entryForm.addEventListener('submit', addItem);
+checklist.addEventListener('click', checked);
