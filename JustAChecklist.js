f715b6a35b826b9f08a42fdf8e2519ef9ccdc866
Dave McCalmont <dave_mccalmont@hotmail.com>
2018-04-11 23:48:13 -0500

    Checked items will now move from initial list to completed list

--- JustAChecklist.js
+++ JustAChecklist.js
@@ -2,27 +2,33 @@ const item = document.querySelector('input[type=text]');
 const entryForm = document.querySelector('form');
 const checklist = document.querySelector('.checklist');
 const checkedBoxes = document.querySelector('.checkedBoxes');
-const toDoItems = [];
-const compItems = [];
+let toDoItems = [];
+let compItems = [];
 
 function addItem(e){
   e.preventDefault();
   toDoItems.push(item.value);
-  checklist.innerHTML = toDoItems.map((toDoItem,i) => {
-      return `
-      <li>
-        <input type="checkbox" id="toDoItem${i}" data-index=${i}>
-        <label for="toDoItem${i}">${toDoItem}</label>
-       </li>
-          `;
-      }).join('');
   entryForm.reset();
+  updateList();
+};
+
+function updateList() {
+  checklist.innerHTML = toDoItems.map((toDoItem,i) => {
+    return `
+    <li>
+      <input type="checkbox" id="toDoItem${i}" data-index=${i}>
+      <label for="toDoItem${i}">${toDoItem}</label>
+    </li>
+    `;
+  }).join('');
+
 };
 
 function checked(e) {
-  let indexChecked = e.target.dataset.index;
-  console.log(indexChecked);
-  if(e.target.checked) {
+  let indexChecked;
+  let updatedToDo;
+    if(e.target.checked) {
+    indexChecked = parseInt(e.target.dataset.index);
     compItems.push(toDoItems[indexChecked]);
     checkedBoxes.innerHTML = compItems.map((compItem,i) => {
       return `
@@ -32,21 +38,14 @@ function checked(e) {
       </li>
           `;
     }).join('');
+    updatedToDo = [...toDoItems.slice(0,indexChecked), ...toDoItems.slice(indexChecked + 1)];
+    toDoItems = updatedToDo;
+    updateList();
+  };
+};
 
-    // const leftToDo = [...toDoItems.slice(0,indexChecked),
-    //                   ...toDoItems.slice(indexChecked+1)];
-    // console.log(leftToDo);
-  }
-}
 
 
-// function checked(e) {
-//   if(e.target.checked) {
-//     console.log('I am checked!');
-//   } else {
-//     console.log('I am not checked!');
-//   };
-// };
 
 entryForm.addEventListener('submit', addItem);
 checklist.addEventListener('click', checked);
