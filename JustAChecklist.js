6639eee12e15969581c299047cdbc2a044b8c570
Dave McCalmont <dave_mccalmont@hotmail.com>
2018-05-10 01:16:22 -0500

    Added the ability to uncheck a completed item and return it to the to-do list

--- JustAChecklist.js
+++ JustAChecklist.js
@@ -9,10 +9,10 @@ function addItem(e){
   e.preventDefault();
   toDoItems.push(item.value);
   entryForm.reset();
-  updateList();
+  updateToDoItems();
 };
 
-function updateList() {
+function updateToDoItems() {
   checklist.innerHTML = toDoItems.map((toDoItem,i) => {
     return `
     <li>
@@ -21,31 +21,51 @@ function updateList() {
     </li>
     `;
   }).join('');
-
 };
+
+function updateCheckedItems() {
+  checkedBoxes.innerHTML = compItems.map((compItem,i) => {
+    return `
+    <li>
+      <input type="checkbox" id="compItem${i}" data-index=${i} checked>
+      <label for="compItem${i}">${compItem}</label>
+    </li>
+    `;
+  }).join('');
+};
+
 
 function checked(e) {
   let indexChecked;
   let updatedToDo;
     if(e.target.checked) {
-    indexChecked = parseInt(e.target.dataset.index);
-    compItems.push(toDoItems[indexChecked]);
-    checkedBoxes.innerHTML = compItems.map((compItem,i) => {
-      return `
-      <li>
-        <input type="checkbox" checked=true id="compItem${i}">
-        <label for="compItem${i}">${compItem}</label>
-      </li>
-          `;
-    }).join('');
-    updatedToDo = [...toDoItems.slice(0,indexChecked), ...toDoItems.slice(indexChecked + 1)];
-    toDoItems = updatedToDo;
-    updateList();
-  };
+      console.log(e.target.dataset.index);
+      indexChecked = parseInt(e.target.dataset.index);
+      compItems.push(toDoItems[indexChecked]);
+      updatedToDo = [...toDoItems.slice(0,indexChecked), ...toDoItems.slice(indexChecked + 1)];
+      toDoItems = updatedToDo;
+      updateCheckedItems();
+      updateToDoItems();
+  }
 };
+
+function unchecked(e) {
+  let indexUnchecked;
+  let updatedCompItems;
+    if(!e.target.checked) {
+      console.log(e.target.dataset.index);
+      indexUnchecked = parseInt(e.target.dataset.index);
+      toDoItems.push(compItems[indexUnchecked]);
+      updatedCompItems = [...compItems.slice(0,indexUnchecked), ...compItems.slice(indexUnchecked + 1)];
+      compItems = updatedCompItems;
+      updateCheckedItems();
+      updateToDoItems();
+    }
+  };
 
 
 
 
 entryForm.addEventListener('submit', addItem);
 checklist.addEventListener('click', checked);
+checkedBoxes.addEventListener('click', unchecked);
