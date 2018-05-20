9373ba79d5d0fe5746e57159cbd3d909e7ec83b0
Dave McCalmont <dave_mccalmont@hotmail.com>
2018-05-20 01:09:49 -0500

    Refactored updateToDoItems and updateCheckedItems and combined them both into one function: updateItems

--- JustAChecklist.js
+++ JustAChecklist.js
@@ -2,65 +2,89 @@ const item = document.querySelector('input[type=text]');
 const entryForm = document.querySelector('form');
 const checklist = document.querySelector('.checklist');
 const checkedBoxes = document.querySelector('.checkedBoxes');
+
 let toDoItems = [];
 let compItems = [];
+let boxChecked = false;
 
 function addItem(e){
   e.preventDefault();
   toDoItems.push(item.value);
   entryForm.reset();
-  updateToDoItems();
+  updateItems(checklist,toDoItems);
 };
 
-function updateToDoItems() {
-  checklist.innerHTML = toDoItems.map((toDoItem,i) => {
+function updateItems(list,items) {
+  list.innerHTML = items.map((item,i) => {
     return `
-    <li>
-      <input type="checkbox" id="toDoItem${i}" data-index=${i}>
-      <label for="toDoItem${i}">${toDoItem}</label>
-    </li>
-    `;
+      <li>
+        <input type="checkbox" id="item${i}" data-index=${i} ${boxChecked ? "checked" : ""}>
+        <label for= "item${i}">${item}</label>
+      </li>
+      `;
   }).join('');
 };
 
-function updateCheckedItems() {
-  checkedBoxes.innerHTML = compItems.map((compItem,i) => {
-    return `
-    <li>
-      <input type="checkbox" id="compItem${i}" data-index=${i} checked>
-      <label for="compItem${i}">${compItem}</label>
-    </li>
-    `;
-  }).join('');
-};
+
+// function updateToDoItems() {
+//   checklist.innerHTML = toDoItems.map((toDoItem,i) => {
+//     return `
+//     <li>
+//       <input type="checkbox" data-index=${i} id="toDoItem${i}">
+//       <label for="toDoItem${i}">${toDoItem}</label>
+//     </li>
+//     `;
+//   }).join('');
+// };
+
+// function updateCheckedItems() {
+//   checkedBoxes.innerHTML = compItems.map((compItem,i) => {
+//     return `
+//     <li>
+//       <input type="checkbox" data-index=${i} id="compItem${i}" checked>
+//       <label for="compItem${i}">${compItem}</label>
+//     </li>
+//     `;
+//   }).join('');
+// };
 
 
 function checked(e) {
   let indexChecked;
   let updatedToDo;
     if(e.target.checked) {
-      console.log(e.target.dataset.index);
+      console.log(e.target);
       indexChecked = parseInt(e.target.dataset.index);
       compItems.push(toDoItems[indexChecked]);
       updatedToDo = [...toDoItems.slice(0,indexChecked), ...toDoItems.slice(indexChecked + 1)];
       toDoItems = updatedToDo;
-      updateCheckedItems();
-      updateToDoItems();
-  }
+      debugger
+      boxChecked = true;
+      updateItems(checkedBoxes,compItems);
+      boxChecked = false;
+      updateItems(checklist,toDoItems);
+  };
 };
 
 function unchecked(e) {
   let indexUnchecked;
   let updatedCompItems;
     if(!e.target.checked) {
-      console.log(e.target.dataset.index);
-      indexUnchecked = parseInt(e.target.dataset.index);
+      console.log('unchecked');
+      console.log(e.target);
+      indexUnchecked = parseInt(e.target.dataset.index); //returning NAN when label is clicked on
       toDoItems.push(compItems[indexUnchecked]);
       updatedCompItems = [...compItems.slice(0,indexUnchecked), ...compItems.slice(indexUnchecked + 1)];
       compItems = updatedCompItems;
-      updateCheckedItems();
-      updateToDoItems();
-    }
+      console.log(e.target.dataset.index);
+      console.log(toDoItems);
+      debugger
+      updateItems(checklist,toDoItems);
+      debugger
+      boxChecked = true;
+      updateItems(checkedBoxes,compItems);
+      boxChecked = false;
+    };
   };
 
 
